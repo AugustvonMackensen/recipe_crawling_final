bf0bd80d1a2b7041bf44989102e7f293a31d97c6
Se Min Lee <bonaparte_19th@hotmail.com>
2022-11-30 13:06:34 +0900

    검색어 붙어나오는거 수정

--- recipe_crawling.py
+++ recipe_crawling.py
@@ -20,13 +20,18 @@ def get_recipe():
     print(food_list)
 
     recipe_dict = {}
-
     driver.get('https://www.10000recipe.com/index.html')
 
     for idx in range(len(food_list)):
-        keyword = food_list[idx]
         try:
-            driver.find_element(By.CSS_SELECTOR, '#srhRecipeText').send_keys(keyword)
+            search = driver.find_element(By.CSS_SELECTOR, '#srhRecipeText')
+
+            # 키워드 붙어서 나오는 현상 방지
+            search.clear()
+
+            keyword = food_list[idx]
+            print(keyword)
+            search.send_keys(keyword)
             driver.find_element(By.CSS_SELECTOR, '#frmTopRecipeSearch > div > span > button').click()
             for n in range(1, 30):
                 driver.find_element(By.CSS_SELECTOR, '#contents_area_full > ul > ul > li:nth-child(' + str(n) +
@@ -38,14 +43,14 @@ def get_recipe():
                 recipe_content = ''
                 steps = soup.find_all('div', {'class': 'view_step_cont'})
                 for s in steps:
-                    recipe_content = recipe_content + s.find('div', {'class': 'media-body'}).text.replace('\n', '')
-
+                    recipe_content = recipe_content + s.find('div', {'class': 'media-body'}).text.replace('\n', ' ')
                 recipe_imgpath = driver.find_element(By.CSS_SELECTOR, '#main_thumbs').get_attribute('src')
                 print([recipe_title, recipe_content, recipe_imgpath])
-                if recipe_content is None:
+                if recipe_content == '':
                     continue
                 recipe_dict[recipe_title] = [recipe_title, recipe_content, recipe_imgpath]
                 driver.back()
+
         except Exception:
             continue
     print(recipe_dict)
