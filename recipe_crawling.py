bc66970fd6eed9972f48562d0b3b5ac633630ad7
Se Min Lee <bonaparte_19th@hotmail.com>
2022-12-01 15:54:09 +0900

    코드작업, nosuchElement 문제 해결...

--- recipe_crawling.py
+++ recipe_crawling.py
@@ -4,12 +4,16 @@ import json
 from bs4 import BeautifulSoup
 from selenium.common import NoSuchElementException
 from selenium import webdriver as wd
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 
 options = wd.ChromeOptions
 driver = wd.Chrome(options=options(), service=Service(ChromeDriverManager().install()))
+
 
 def get_recipe():
     data = open('./kor_eng_foodtitle.json', 'r', encoding='utf-8').read()
@@ -25,31 +29,35 @@ def get_recipe():
     for idx in range(len(food_list)):
         try:
             search = driver.find_element(By.CSS_SELECTOR, '#srhRecipeText')
-
-            # 키워드 붙어서 나오는 현상 방지
             search.clear()
 
             keyword = food_list[idx]
             print(keyword)
             search.send_keys(keyword)
-            driver.find_element(By.CSS_SELECTOR, '#frmTopRecipeSearch > div > span > button').click()
-            for n in range(1, 30):
-                driver.find_element(By.CSS_SELECTOR, '#contents_area_full > ul > ul > li:nth-child(' + str(n) +
-                                    ') > div > a').click()
+            WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#frmTopRecipeSearch  \
+                                                        > div > span > button'))).click()
+            for n in range(1, 40):
+                time.sleep(5)
+                element = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                                      '#contents_area_full > ul > ul \
+                                                                                      > li:nth-child(' + str(
+                                                                                        n) + ') > div > a')))
+                # Element is not clickable 방지 코드
+                driver.execute_script('arguments[0].click();', element)
                 recipe_title = driver.find_element(By.CSS_SELECTOR, '#contents_area > div.view2_summary.st3 > h3').text
-
                 soup = BeautifulSoup(driver.page_source, 'html.parser')
                 # 요리 순서 처리
                 steps = soup.find_all('div', {'class': 'view_step_cont'})
+                recipe_content = ''
                 for s in steps:
-                    recipe_content = recipe_content + s.find('div', {'class': 'media-body'}).text.replace('\n', ' ')
+                    recipe_content = recipe_content + s.find('div', {'class': 'media-body'}).text
+                    if recipe_content == '':
+                        continue
                 recipe_imgpath = driver.find_element(By.CSS_SELECTOR, '#main_thumbs').get_attribute('src')
                 print([recipe_title, recipe_content, recipe_imgpath])
-                if recipe_content == '':
-                    continue
                 recipe_dict[recipe_title] = [recipe_title, recipe_content, recipe_imgpath]
                 driver.back()
-
-        except Exception:
+        except Exception as Msg:
+            print(Msg)
             continue
     print(recipe_dict)
